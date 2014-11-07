5f75b93de3a1f68331f10abb46061e421abb84eb
bringsyrup <rubymspring@gmail.com>
2014-11-07 14:52:54 -0500

    fixed again yay

--- pystack.sh
+++ pystack.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 PYSCRIPT=$1
-if [$PYSCRIPT]; then
-    python $PYSCRIPT 2>&1 | python out.py
+if [ $PYSCRIPT ]
+then
+    (python $PYSCRIPT 2>&1 | python out.py)
 else 
     python
 fi
