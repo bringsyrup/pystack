ceb286b7e3e2881cefa0e05dd411d28039aafbd1
bringsyrup <rubymspring@gmail.com>
2014-11-13 20:31:16 -0500

    added some error test code

--- out.py
+++ out.py
@@ -1,6 +1,24 @@
 import sys
+import re
 
-stderr = []
-for line in sys.stdin.readlines():
-    stderr.append(line)
-print stderr
+def getErrs():
+    stderr = []
+    for line in sys.stdin.readlines():
+        err = re.sub('[\n]', '', line)
+        stderr.append(err.strip(" "))
+    return stderr[-1]
+
+def parseErrs():
+    stderr = getErrs()
+    errRoot = stderr[-1]
+    if "TypeError" in errRoot:
+        print errRoot
+    elif "IndexError" in errRoot:
+        print errRoot
+    elif "NameError" in errRoot:
+        print errRoot
+    elif "UnboundLocalError" in errRoot:
+        print errRoot
+
+if __name__=="__main__":
+    parseErrs()
