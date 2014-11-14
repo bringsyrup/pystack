4d9ab3b3cfa50be0f2f5ba9576182d4f5b444b6d
AnnaKnapp <anna.knapp@students.olin.edu>
2014-11-14 14:42:55 -0500

    added searching

--- out2.py
+++ out2.py
@@ -20,7 +20,13 @@ def parseErrs():
         print errRoot
     elif "UnboundLocalError" in errRoot:
         print errRoot
-
+        
+def searchso(term1,term2):
+    user_api_key = '5se*FOHNKmiw3H9miisy8w(('
+    so = stackexchange.Site(stackexchange.StackOverflow, app_key = user_api_key, impose_throttling = True)
+    qs = so.search_advanced(q=term1, tagged=['python'], body=term2)
+    for q in qs:
+        print(q.id, q.title)
 
 if __name__=="__main__":
     parseErrs()
