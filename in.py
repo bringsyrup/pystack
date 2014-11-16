357958f2e30b7dbc1b17c4591e7274470a323bb2
bringsyrup <rubymspring@gmail.com>
2014-11-15 19:08:23 -0500

    deleted old things and repeats

--- in.py
+++
@@ -1,4 +0,0 @@
-hello = "Hello World"
-bye = []
-for i in range(len(hello) + 1):
-    bye.append(hello[i])
--- indexErr.py
+++
@@ -1,5 +0,0 @@
-hello = "Hello World"
-hello2=(1,2)
-bye = []
-for i in range(len(hello2) + 1):
-    bye.append(hello2[i])
