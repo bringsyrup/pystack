b0cfd3fccd968af4e06e0cc3c9e3dfe5f4efb6f1
AnnaKnapp <anna.knapp@students.olin.edu>
2014-11-17 23:32:00 -0500

    now it gets the top 5(by rating) of the entire search

--- pystackpy/pystack.py
+++ pystackpy/pystack.py
@@ -23,17 +23,22 @@ def getErrs():
 
 def searchSO(term1,term2, limit):
     user_api_key = '5se*FOHNKmiw3H9miisy8w(('
-    so = stackexchange.Site(stackexchange.StackOverflow, app_key = user_api_key, impose_throttling = True)
+    so = stackexchange.Site(stackexchange.StackOverflow, app_key = user_api_key, impose_throttling = False)
+    so.throttle_stop = False
     qs = so.search_advanced(q=term1, tagged=['python'], body=term2)
     #for q in qs:
     expanded_q = []
-    for i in range(limit):
-        q = so.question(qs[i].id, body=True, filter = "!b0OfMwwD.s*79x")
-        expanded_q.append(q)
+    # counter = 0
+    # for q in qs:
+    #     print q.url
+    #     counter = counter + 1
+    # print counter
+    for q in qs:
+        r = so.question(q.id, body = True, filter = "!b0OfMwwD.s*79x") 
+        expanded_q.append(r)
     expanded_q.sort(key=lambda x: x.up_vote_count, reverse=True)
-    for y in expanded_q:
-        print y.url
-
+    for i in range(limit):
+        print expanded_q[i].url
 def getSO(limit):
     searchSO(getErrs(), 'tuple', limit)
 
