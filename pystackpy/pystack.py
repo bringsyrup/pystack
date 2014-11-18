e1fd6bc60dc636c07c63e1f95445a80f1f7bf795
bringsyrup <rubymspring@gmail.com>
2014-11-18 15:09:11 -0500

    added optional term input

--- pystackpy/pystack.py
+++ pystackpy/pystack.py
@@ -2,12 +2,12 @@ import sys
 import re
 import stackexchange
 import os
+import argparse
 
 def getErrs():
     stderr = []
     for line in sys.stdin.readlines():
         stderr.append(re.sub('[\n]', '', line).strip(" "))
-    print stderr
     return stderr[-1]
 
 def getDiff():
@@ -32,26 +32,30 @@ def getDiff():
 #        print errRoot
 
 def searchSO(term1,term2, limit):
+    print term1, term2
     user_api_key = '5se*FOHNKmiw3H9miisy8w(('
-    so = stackexchange.Site(stackexchange.StackOverflow, app_key = user_api_key, impose_throttling = False)
+    so = stackexchange.Site(stackexchange.StackOverflow, app_key=user_api_key, impose_throttling=False)
     so.throttle_stop = False
     qs = so.search_advanced(q=term1, tagged=['python'], body=term2)
-    expanded_q = []
-    for q in qs:
-        r = so.question(q.id, body = True, filter = "!b0OfMwwD.s*79x") 
-        expanded_q.append(r)
+    #for q in qs:
+    #    r = so.question(q.id, body=True, filter="!b0OfMwwD.s*79x") 
+    ql = list(qs)
+    ql.sort(key=lambda x: x.score, reverse=True)
     for i in range(limit):
-        try:
-            q = so.question(qs[i].id, body=True, filter = "!b0OfMwwD.s*79x")
-        except IndexError:
-            break
-        expanded_q.append(q)
-    expanded_q.sort(key=lambda x: x.up_vote_count, reverse=True)
-    for i in range(limit):
-        print expanded_q[i].url
-def getSO(limit):
-    searchSO(getErrs(), 'tuple', limit)
+        print ql[i].url
+
+def getSO(limit, term2):
+    searchSO(getErrs(), term2, limit)
 
 if __name__=="__main__":
-    getSO(5)
-    print getDiff()
+    parser = argparse.ArgumentParser(description='pystack')
+    parser.add_argument('search_term',
+            type = str, 
+            nargs = '?',
+            help = 'optional search term string!'
+            )
+    args = parser.parse_args()
+    getSO(5, args.search_term)
+    
+    getDiff()
+
