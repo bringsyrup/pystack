8c5e2bdd6033b8fe5040d681149d1d7dcbea10c1
AnnaKnapp <anna.knapp@students.olin.edu>
2014-11-18 14:55:56 -0500

    added body to top 10 results

--- pystackpy/pystack.py
+++ pystackpy/pystack.py
@@ -36,12 +36,14 @@ def searchSO(term1,term2, limit):
     so = stackexchange.Site(stackexchange.StackOverflow, app_key = user_api_key, impose_throttling = False)
     so.throttle_stop = False
     qs = so.search_advanced(q=term1, tagged=['python'], body=term2, accepted=True)
-    #     r = so.question(q.id, body = True, filter = "!b0OfMwwD.s*79x") 
-
     ql = list(qs)
     ql.sort(key=lambda x: x.score, reverse=True)
     for i in range(limit):
         print ql[i].url
+    bodylist = []
+    for i in range(10):
+        r = so.question(ql[i].id, body = True) 
+        bodylist.append(r)
 def getSO(limit):
     searchSO(getErrs(), 'tuple', limit)
 
